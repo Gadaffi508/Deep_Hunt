37ca6b260d173a5a9f554b4852e5b9f3e7b3f79b
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-06-12 22:31:32 +0300

    Update

--- Assets/SecondLevel/Scripts/GameScripts/SpawnController.cs
+++ Assets/SecondLevel/Scripts/GameScripts/SpawnController.cs
@@ -35,70 +35,69 @@ public class SpawnController : MonoBehaviour
     }
     IEnumerator Timer()
     {
-        if (waveSend)
+
+        random = Random.Range(0, spawnPointsSwim.Length);
+        Instantiate(enemySwim, spawnPointsSwim[random].position, Quaternion.identity);
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyLittleSwim, spawnPointsSwim[random].position, Quaternion.identity);
+            yield return new WaitForSeconds(2f);
+        }
+
+        yield return new WaitForSeconds(12f);
+
+        for (int a = 0; a < 5; a++)
         {
             random = Random.Range(0, spawnPointsSwim.Length);
+            Instantiate(drunkEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
+            yield return new WaitForSeconds(3.75f);
+        }
+
+        yield return new WaitForSeconds(18f);
+
+        for (int a = 0; a < 2; a++)
+        {
+            random = Random.Range(0, spawnPointsSwim.Length);
+            yield return new WaitForSeconds(3f);
             Instantiate(enemySwim, spawnPointsSwim[random].position, Quaternion.identity);
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 2; i++)
             {
                 Instantiate(enemyLittleSwim, spawnPointsSwim[random].position, Quaternion.identity);
                 yield return new WaitForSeconds(2f);
             }
-
-            yield return new WaitForSeconds(12f);
-
-            for (int a = 0; a < 5; a++)
+            for (int b = 0; b < 2; b++)
             {
-                random = Random.Range(0, spawnPointsSwim.Length);
+                Instantiate(flyEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
+                yield return new WaitForSeconds(2f);
                 Instantiate(drunkEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
-                yield return new WaitForSeconds(3.75f);
+                yield return new WaitForSeconds(2f);
             }
-
-            yield return new WaitForSeconds(18f);
-
-            for (int a = 0; a < 2; a++)
-            {
-                random = Random.Range(0, spawnPointsSwim.Length);
-                yield return new WaitForSeconds(3f);
-                Instantiate(enemySwim, spawnPointsSwim[random].position, Quaternion.identity);
-                for (int i = 0; i < 2; i++)
-                {
-                    Instantiate(enemyLittleSwim, spawnPointsSwim[random].position, Quaternion.identity);
-                    yield return new WaitForSeconds(2f);
-                }
-                for (int b = 0; b < 2; b++)
-                {
-                    Instantiate(flyEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
-                    yield return new WaitForSeconds(2f);
-                    Instantiate(drunkEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
-                    yield return new WaitForSeconds(2f);
-                }
-                yield return new WaitForSeconds(3f);
-            }
-
-            yield return new WaitForSeconds(15f);
-
-            for (int a = 0; a < 4; a++)
-            {
-                random = Random.Range(0, spawnPointsSwim.Length);
-                Instantiate(drunkEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
-                yield return new WaitForSeconds(3f);
-                Instantiate(enemySwim, spawnPointsSwim[random].position, Quaternion.identity);
-                for (int i = 0; i < 2; i++)
-                {
-                    Instantiate(enemyLittleSwim, spawnPointsSwim[random].position, Quaternion.identity);
-                    yield return new WaitForSeconds(2f);
-                }
-                for (int b = 0; b < 2; b++)
-                {
-                    Instantiate(flyEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
-                    yield return new WaitForSeconds(3f);
-                }
-                yield return new WaitForSeconds(3f);
-                
-            }
-
-            GameManager.Instance.finished = true;
+            yield return new WaitForSeconds(3f);
         }
+
+        yield return new WaitForSeconds(15f);
+
+        for (int a = 0; a < 4; a++)
+        {
+            random = Random.Range(0, spawnPointsSwim.Length);
+            Instantiate(drunkEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
+            yield return new WaitForSeconds(3f);
+            Instantiate(enemySwim, spawnPointsSwim[random].position, Quaternion.identity);
+            for (int i = 0; i < 2; i++)
+            {
+                Instantiate(enemyLittleSwim, spawnPointsSwim[random].position, Quaternion.identity);
+                yield return new WaitForSeconds(2f);
+            }
+            for (int b = 0; b < 2; b++)
+            {
+                Instantiate(flyEnemy, SpawnPointsFlay[random].position, Quaternion.identity);
+                yield return new WaitForSeconds(3f);
+            }
+            Debug.Log("Bitti");
+            yield return new WaitForSeconds(3f);
+
+        }
+        yield return new WaitForSeconds(5f);
+        GameManager.Instance.finished = true;
     }
 }
